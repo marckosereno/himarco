2861f9075ff19add1fad5b4c8966c738548834f2
marckosereno <140083243+marckosereno@users.noreply.github.com>
2025-12-23 08:42:51 -0600

    Add Home component with ChatWindow

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import ChatWindow from '@/components/ChatWindow'
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen overflow-hidden">
+      <ChatWindow />
+    </main>
+  )
+}
